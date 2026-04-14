827e8eadaf20af5929be9b9c47dcca96e59a6137
NITIN VISHWAKARMA <37566535+nitinvishwakarma@users.noreply.github.com>
2026-04-14 12:20:04 +0530

    Update 04-pareto-analysis.sql

--- 04-pareto-analysis.sql
+++ 04-pareto-analysis.sql
@@ -1,3 +1,53 @@
+-- This is the new query--updated
+-- ==============================================================================
+-- PROJECT ALPHA: Pareto (80/20) Inventory & Revenue Analysis
+-- Purpose: Calculates cumulative revenue percentages to identify top-performing 
+-- product categories using advanced Window Functions.
+-- ==============================================================================
+CREATE OR ALTER VIEW vw_Pareto_Analysis_cumulative AS
+
+WITH CategoryRevenue AS (
+    -- STEP 1: Calculate total revenue per category
+
+	SELECT	oit.ProductID,p.CategoryName, SUM(oit.price) AS TotalRevenue
+	FROM Order_Items AS oit
+	JOIN Products AS p ON oit.ProductID = p.ProductID
+	GROUP BY oit.ProductID,p.CategoryName
+	),
+	
+	CumulativeTotals AS (
+    -- STEP 2: Calculate the running total and the grand total
+	SELECT	CategoryName,
+			TotalRevenue,
+			-- Running total of revenue
+			SUM(TotalRevenue) over (ORDER BY TotalRevenue DESC ROWS UNBOUNDED PRECEDING) AS RunningTotalRevenue,
+			-- Grand Total revenue for percentage calc
+			SUM(TotalRevenue) over() AS GrandTotalRevenue
+
+	FROM CategoryRevenue
+	)
+
+-- STEP 3: Calculate the cumulative percentage
+	SELECT	CategoryName,
+			TotalRevenue,
+			RunningTotalRevenue,
+			GrandTotalRevenue,
+			-- Cumulative percentage (the Pareto line) and round it to 2 decimal places
+			CAST((RunningTotalRevenue / GrandTotalRevenue) * 100.0 AS DECIMAL(5,2)) AS CumulativePercentage
+
+	FROM  CumulativeTotals;
+
+-- ==============================================================================
+-- TEST THE VIEW
+-- ==============================================================================
+-- Run the line below. Look at the "CumulativePercentage" column!
+
+SELECT * FROM vw_Pareto_Analysis_cumulative ORDER BY TotalRevenue DESC
+
+
+
+
+/*
 -- Pareto Analysis (80/20 Rule)
 -- Purpose: Identify vital few categories driving majority of revenue
 -- Use Case: Inventory optimization, ABC analysis
@@ -53,4 +103,4 @@ ORDER BY
 
 -- Usage: Identify which categories to prioritize for next quarter
 -- SELECT * FROM vw_Pareto_Revenue_Analysis WHERE ABC_Classification LIKE 'A%';
-
+*/
